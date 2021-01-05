846ace2568b6364f64142d5a31ff8e0806698f86
yujin mac <kimyu28jin@gmail.com>
2021-01-05 21:44:10 +0900

    logo,signup 컴포넌트 생성후 import

---
+++ front/pages/signup.js
@@ -0,0 +1,13 @@
+import Logo from "../components/logo";
+import SignupForm from "../components/signupForm";
+
+const Signup = () => {
+  return (
+    <>
+      <Logo />
+      <SignupForm />
+    </>
+  );
+};
+
+export default Signup;
