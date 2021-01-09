ce572e195ea7e43290af42f3a35125834bef9d2e
yujin mac <kimyu28jin@gmail.com>
2021-01-09 20:44:40 +0900

    배경 gradient 적용

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,14 +1,20 @@
 import Logo from "../components/logo";
 import SignupForm from "../components/signupForm";
+import styled from "styled-components";
+
+const GradientBg = styled.section`
+  height: 100vh;
+  background: linear-gradient(#2f69ad, #824adc);
+`;
 
 const Signup = () => {
   return (
-    <>
-      <div style={{ textAlign: "center", marginTop: "2em" }}>
+    <GradientBg>
+      <div style={{ textAlign: "center", paddingTop: "2em" }}>
         <Logo isForm />
       </div>
       <SignupForm />
-    </>
+    </GradientBg>
   );
 };
 
