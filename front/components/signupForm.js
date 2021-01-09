dd13d4332b2d06a27f5745042036fc7a85d5d105
yujin mac <kimyu28jin@gmail.com>
2021-01-09 20:30:13 +0900

    signupForm의 input, btn 스타일링 추가

--- front/components/signupForm.js
+++ front/components/signupForm.js
@@ -4,6 +4,7 @@ import { HiLockClosed } from "react-icons/Hi";
 import { useForm } from "react-hook-form";
 import { useRef } from "react";
 import Input from "../styles/input";
+import styled from "styled-components";
 
 const SignupForm = () => {
   const { register, handleSubmit, watch, errors } = useForm();
@@ -12,10 +13,33 @@ const SignupForm = () => {
   password.current = watch("password");
 
   const onSubmit = (data) => console.log(data);
+
+  const InputWrapper = styled.div`
+    position: relative;
+  `;
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <FiMail />
+    <form
+      style={{
+        paddingTop: "4em",
+        margin: "0 auto",
+        width: "40%",
+        minWidth: "300px",
+        maxWidth: "400px",
+      }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <InputWrapper>
+        <FiMail
+          style={{
+            fontSize: "1.4rem",
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+            color: "rgba(255,255,255,0.8)",
+            marginLeft: "1em",
+          }}
+        />
         <Input
           name="email"
           type="email"
@@ -26,12 +50,21 @@ const SignupForm = () => {
             maxLength: 30,
           })}
         />
-      </div>
+      </InputWrapper>
       {errors.email?.type === "required" && <p>이메일을 입력해주세요.</p>}
       {errors.email?.type === "pattern" && <p>이메일 형식이 맞지 않습니다.</p>}
       {errors.email?.type === "maxLength" && <p>이메일을 확인해주세요.</p>}
-      <div>
-        <BsFillPersonFill />
+      <InputWrapper>
+        <BsFillPersonFill
+          style={{
+            fontSize: "1.4rem",
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+            color: "rgba(255,255,255,0.8)",
+            marginLeft: "1em",
+          }}
+        />
         <Input
           name="nickname"
           placeholder="Nickname..."
@@ -40,26 +73,44 @@ const SignupForm = () => {
             minLength: 2,
           })}
         />
-      </div>
+      </InputWrapper>
       {errors.nickname?.type === "required" && <p>닉네임을 입력해주세요.</p>}
       {errors.nickname?.type === "minLength" && (
         <p>닉네임은 2자이상 입력해주세요.</p>
       )}
-      <div>
-        <HiLockClosed />
+      <InputWrapper>
+        <HiLockClosed
+          style={{
+            fontSize: "1.4rem",
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+            color: "rgba(255,255,255,0.8)",
+            marginLeft: "1em",
+          }}
+        />
         <Input
           name="password"
           type="password"
           placeholder="Password..."
           ref={register({ required: true, minLength: 6 })}
         />
-      </div>
+      </InputWrapper>
       {errors.password?.type === "required" && <p>비밀번호를 입력해주세요.</p>}
       {errors.password?.type === "minLength" && (
         <p>비밀번호는 6자이상 입력해주세요.</p>
       )}
-      <div>
-        <HiLockClosed />
+      <InputWrapper>
+        <HiLockClosed
+          style={{
+            fontSize: "1.4rem",
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+            color: "rgba(255,255,255,0.8)",
+            marginLeft: "1em",
+          }}
+        />
         <Input
           name="confirm_password"
           type="password"
@@ -70,7 +121,7 @@ const SignupForm = () => {
             validate: (value) => value === password.current,
           })}
         />
-      </div>
+      </InputWrapper>
       {errors.confirm_password?.type === "required" && (
         <p>비밀번호 확인이 필요합니다.</p>
       )}
@@ -81,7 +132,20 @@ const SignupForm = () => {
         <p>비밀번호가 일치하지 않습니다.</p>
       )}
 
-      <Input isBtn value="가입하기" type="submit" />
+      <Input
+        isBtn
+        value="가입하기"
+        type="submit"
+        style={{
+          position: "absolute",
+          left: "50%",
+          transform: "translateX(-50%)",
+          bottom: "5em",
+          width: "40%",
+          minWidth: "300px",
+          maxWidth: "400px",
+        }}
+      />
     </form>
   );
 };
--- front/styles/input.js
+++ front/styles/input.js
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 const StyledInput = styled.input`
   width: 100%;
-  padding: 1em 3em;
+  padding: 1em 3.5em;
+  margin: 15px 0;
   border: 1px solid #fff;
-  border-radius: 20px;
+  border-radius: 25px;
+  font-size: 1rem;
+  outline: none;
   color: ${({ isBtn }) => (isBtn ? `#fff` : `rgba(255, 255, 255, 0.4)`)};
   background: ${({ isBtn }) => (isBtn ? `rgba(255, 255, 255, 0.1)` : `none`)};
+  cursor: ${({ isBtn }) => (isBtn ? `pointer` : `text`)};
 `;
 
 const Input = ({ ...props }) => {
