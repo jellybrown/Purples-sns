552967cd726f694489b385c7ed864a28aaa83d79
yujin mac <kimyu28jin@gmail.com>
2021-01-18 19:06:25 +0900

    mainHome 컴포넌트 배치 완료

--- front/components/postCardImg.js
+++ front/components/postCardImg.js
@@ -6,6 +6,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   touch-action: none !important;
+  overflow: hidden;
 `;
 const PostCardImg = () => {
   // post image 받기
@@ -22,8 +23,15 @@ const PostCardImg = () => {
       src:
         "https://raw.githubusercontent.com/pegsbenedict/demos/master/img/slider2-funders.jpg",
     },
+    {
+      src:
+        "https://previews.123rf.com/images/roxanabalint/roxanabalint1312/roxanabalint131200148/24476498-demo-grunge-rubber-stamp-on-white.jpg",
+    },
   ];
   const [currentSlide, setCurrentSlide] = useState(0);
+  {
+    /* 아직 쓸일은 없음, 나중에 있을지도 */
+  }
 
   return (
     <Slick
@@ -37,7 +45,14 @@ const PostCardImg = () => {
     >
       {images.map((img) => (
         <Wrapper>
-          <img src={img.src} style={{ width: "600px" }} />
+          <img
+            src={img.src}
+            style={{
+              height: "calc(50vh - 100px)",
+              width: "auto",
+              margin: "0 auto",
+            }}
+          />
         </Wrapper>
       ))}
     </Slick>
