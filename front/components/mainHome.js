d8056fbcd58d02b523addf95b16e0ee1817670b8
yujin mac <kimyu28jin@gmail.com>
2021-01-21 22:40:42 +0900

    반응형 스타일링 적용

--- front/components/mainHome.js
+++ front/components/mainHome.js
@@ -1,24 +1,51 @@
 import Layout from "./layout";
 import MainProfile from "./mainProfile";
 import PostCard from "./postCard";
+import { useMediaQuery } from "react-responsive";
 
 const MainHome = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
+
   return (
     <Layout>
       <div style={{ position: "relative" }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginLeft: "8%",
-            marginTop: "50px",
-          }}
-        >
-          <PostCard />
-        </div>
-        <div style={{ position: "fixed", left: "5rem", top: "7rem" }}>
-          <MainProfile />
-        </div>
+        {isDesktopOrLaptop && (
+          <>
+            <div
+              style={{
+                display: "flex",
+                marginTop: "50px",
+                marginLeft: "calc(10% + 320px)",
+                paddingLeft: "10%",
+              }}
+            >
+              <PostCard /> {/* PostCards 컴포넌트 안에 돌리기 */}
+            </div>
+            <div style={{ position: "fixed", left: "10%", top: "7rem" }}>
+              <MainProfile />
+            </div>
+          </>
+        )}
+        {isTabletOrMobileDevice && (
+          <>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "50px",
+                marginLeft: "0px",
+                paddingLeft: "0px",
+              }}
+            >
+              <PostCard />
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   );
