e972eaed320230fbd499e56eeaead30a850d1aae
yujin mac <kimyu28jin@gmail.com>
2021-01-31 16:19:15 +0900

    서브메뉴를 위한 header 스타일 변경

--- front/components/mainHeader.js
+++ front/components/mainHeader.js
@@ -7,32 +7,43 @@ const MainHeader = () => {
   return (
     <div
       style={{
-        zIndex: "100",
-        alignItems: "center",
-        borderBottom: "1px solid #E1E1E1",
-        padding: "0.8em 3em",
         position: "fixed",
-        top: 0,
-        textAlign: "center",
         width: "100%",
-        background: "#fff",
+        height: "60px",
+        zIndex: "100",
       }}
     >
-      <Link href="/">
-        <a>
-          <AiOutlineHome
-            style={{
-              fontSize: "1.5rem",
-              position: "absolute",
-              left: "4%",
-              top: "50%",
-              transform: "translateY(-50%)",
-            }}
-          />
-        </a>
-      </Link>
-      <Logo style={{ fontSize: "1.8em" }} />
-      <RightMenu />
+      <div
+        style={{
+          alignItems: "center",
+          borderBottom: "1px solid #E1E1E1",
+          padding: "0.8em 3em",
+          position: "absolute",
+          top: 0,
+          textAlign: "center",
+          width: "100%",
+          background: "#fff",
+        }}
+      >
+        <Link href="/">
+          <a>
+            <AiOutlineHome
+              style={{
+                fontSize: "1.5rem",
+                position: "absolute",
+                left: "4%",
+                top: "50%",
+                transform: "translateY(-50%)",
+              }}
+            />
+          </a>
+        </Link>
+        <Logo style={{ fontSize: "1.8em" }} />
+        <RightMenu />
+      </div>
+      <div style={{ position: "absolute", width: "100%", bottom: "-22px" }}>
+        second menu
+      </div>
     </div>
   );
 };
