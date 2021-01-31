eb68141066009a02d4ae8939853b8c34b2e640e9
yujin mac <kimyu28jin@gmail.com>
2021-01-31 15:59:13 +0900

    모바일 버전 메뉴 변경

--- front/components/rightMenu.js
+++ front/components/rightMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsFillPersonPlusFill } from "react-icons/bs";
+import { FcMenu } from "react-icons/fc";
 import { BiSearch } from "react-icons/bi";
 import { FaUserCircle } from "react-icons/fa";
 import { Button, Dropdown, Menu, message } from "antd";
@@ -8,67 +9,114 @@ import { LOGOUT_REQUEST } from "../redux/types";
 import Link from "next/link";
 import Router from "next/router";
 import UserMenu from "./userMenu";
+import { useMediaQuery } from "react-responsive";
 
 const RightMenu = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
   return (
-    <div
-      style={{
-        display: "flex",
-        fontSize: "1.5rem",
-        position: "absolute",
-        right: "6%",
-        top: "50%",
-        display: "flex",
-        transform: "translateY(-50%)",
-      }}
-    >
-      <Link href="/find">
-        <a>
-          <Button
-            color="black"
-            shape="circle"
-            size="large"
-            icon={<BsFillPersonPlusFill style={{ fontSize: "23px" }} />}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              border: "none",
-            }}
-          />
-        </a>
-      </Link>
-      <Link href="/search">
-        <a>
-          <Button
-            color="black"
-            shape="circle"
-            size="large"
-            icon={<BiSearch style={{ fontSize: "23px" }} />}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              border: "none",
-            }}
-          />
-        </a>
-      </Link>
-      <Dropdown overlay={<UserMenu />} trigger="click" placement="bottomCenter">
-        <Button
-          color="black"
-          shape="circle"
-          size="large"
-          icon={<FaUserCircle style={{ fontSize: "23px" }} />}
+    <>
+      {isDesktopOrLaptop && (
+        <div
           style={{
             display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            border: "none",
+            fontSize: "1.5rem",
+            position: "absolute",
+            right: "6%",
+            top: "50%",
+            display: "flex",
+            transform: "translateY(-50%)",
           }}
-        />
-      </Dropdown>
-    </div>
+        >
+          <Link href="/find">
+            <a>
+              <Button
+                color="black"
+                shape="circle"
+                size="large"
+                icon={<BsFillPersonPlusFill style={{ fontSize: "23px" }} />}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  border: "none",
+                }}
+              />
+            </a>
+          </Link>
+          <Link href="/search">
+            <a>
+              <Button
+                color="black"
+                shape="circle"
+                size="large"
+                icon={<BiSearch style={{ fontSize: "23px" }} />}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  border: "none",
+                }}
+              />
+            </a>
+          </Link>
+          <Dropdown
+            overlay={<UserMenu />}
+            trigger="click"
+            placement="bottomCenter"
+          >
+            <Button
+              color="black"
+              shape="circle"
+              size="large"
+              icon={<FaUserCircle style={{ fontSize: "23px" }} />}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                border: "none",
+              }}
+            />
+          </Dropdown>
+        </div>
+      )}
+      {isTabletOrMobileDevice && (
+        <div
+          style={{
+            display: "flex",
+            fontSize: "1.5rem",
+            position: "absolute",
+            right: "4%",
+            top: "50%",
+            display: "flex",
+            transform: "translateY(-50%)",
+          }}
+        >
+          <Dropdown
+            overlay={<UserMenu />}
+            trigger="click"
+            placement="bottomCenter"
+          >
+            <Button
+              color="black"
+              shape="circle"
+              size="large"
+              icon={<FcMenu style={{ fontSize: "23px" }} />}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                border: "none",
+              }}
+            />
+          </Dropdown>
+        </div>
+      )}
+    </>
   );
 };
 
--- front/components/userMenu.js
+++ front/components/userMenu.js
@@ -3,6 +3,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { Menu, message } from "antd";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
+import { useMediaQuery } from "react-responsive";
 
 const StyledMenu = styled(Menu)`
   box-shadow: none !important;
@@ -12,41 +13,137 @@ const StyledMenu = styled(Menu)`
 `;
 
 const UserMenu = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
+
   const dispatch = useDispatch();
 
-  function handleLogoutClick(e) {
+  const handleLogoutClick = (e) => {
     dispatch({
       type: LOGOUT_REQUEST,
     });
     message.info("로그아웃에 성공하였습니다.", 1);
-  }
+  };
 
   const handleProfileClick = (e) => {
     Router.push("/profile");
   };
 
+  const loggedIn = true;
+
   return (
-    <StyledMenu>
-      <Menu.Item
-        key="1"
-        onClick={handleLogoutClick}
-        style={{ borderBottom: "1px solid #ddd" }}
-      >
-        <span>로그아웃</span>
-      </Menu.Item>
-      <Menu.Item
-        key="2"
-        onClick={handleProfileClick}
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <span>프로필</span>
-        <FaUserCircle style={{ fontSize: "1.2rem" }} />
-      </Menu.Item>
-    </StyledMenu>
+    <>
+      {isDesktopOrLaptop && (
+        <StyledMenu>
+          <Menu.Item
+            key="1"
+            onClick={handleLogoutClick}
+            style={{ borderBottom: "1px solid #ddd" }}
+          >
+            <span>로그아웃</span>
+          </Menu.Item>
+          <Menu.Item
+            key="2"
+            onClick={handleProfileClick}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>프로필</span>
+            <FaUserCircle style={{ fontSize: "1.2rem" }} />
+          </Menu.Item>
+        </StyledMenu>
+      )}
+      {isTabletOrMobileDevice && loggedIn ? (
+        <StyledMenu>
+          <Menu.Item
+            key="1"
+            onClick={handleLogoutClick}
+            style={{ borderBottom: "1px solid #ddd" }}
+          >
+            <span>로그아웃</span>
+          </Menu.Item>
+          <Menu.Item
+            key="2"
+            onClick={handleProfileClick}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>프로필</span>
+            <FaUserCircle style={{ fontSize: "1.2rem" }} />
+          </Menu.Item>
+          <Menu.Item
+            key="2"
+            onClick={handleProfileClick}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>친구 찾기</span>
+            <FaUserCircle style={{ fontSize: "1.2rem" }} />
+          </Menu.Item>
+          <Menu.Item
+            key="2"
+            onClick={handleProfileClick}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>게시글 검색</span>
+            <FaUserCircle style={{ fontSize: "1.2rem" }} />
+          </Menu.Item>
+        </StyledMenu>
+      ) : null}
+      {isTabletOrMobileDevice && !loggedIn ? (
+        <StyledMenu>
+          <Menu.Item
+            key="1"
+            onClick={handleLogoutClick}
+            style={{ borderBottom: "1px solid #ddd" }}
+          >
+            <span>로그인</span>
+          </Menu.Item>
+
+          <Menu.Item
+            key="2"
+            onClick={handleProfileClick}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>친구 찾기</span>
+            <FaUserCircle style={{ fontSize: "1.2rem" }} />
+          </Menu.Item>
+          <Menu.Item
+            key="2"
+            onClick={handleProfileClick}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>게시글 검색</span>
+            <FaUserCircle style={{ fontSize: "1.2rem" }} />
+          </Menu.Item>
+        </StyledMenu>
+      ) : null}
+    </>
   );
 };
 
