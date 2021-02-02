010d590b4216d3de9f12f8eb5a21f8773dd51c29
yujin mac <kimyu28jin@gmail.com>
2021-02-02 19:55:44 +0900

    commentReducer 작성

--- front/redux/reducers/commentReducer.js
+++ front/redux/reducers/commentReducer.js
@@ -2,15 +2,23 @@ import {
   ADD_COMMENT_FAILURE,
   ADD_COMMENT_REQUEST,
   ADD_COMMENT_SUCCESS,
+  REMOVE_COMMENT_FAILURE,
+  REMOVE_COMMENT_REQUEST,
+  REMOVE_COMMENT_SUCCESS,
 } from "../types";
 
-const { HYDRATE } = require("next-redux-wrapper");
+import { HYDRATE } from "next-redux-wrapper";
 
 const initialState = {
   addCommentLoading: false,
   addCommentDone: false,
   addCommentError: null,
+  removeCommentLoading: false,
+  removeCommentDone: false,
+  removeCommentError: null,
   comment: "",
+  post: [] //postReducer랑 통합해야할수도
+
 };
 
 const commentReducer = (state = initialState, action) => {
@@ -23,16 +31,37 @@ const commentReducer = (state = initialState, action) => {
         addCommentLoading: true,
       };
     case ADD_COMMENT_SUCCESS:
+        const post = action.payload.postId;
+        const newComment = action.payload;
       return {
         ...state,
-        comment: action.payload,
         addCommentLoading: false,
+        post = post.comments.push(newComment)
       };
     case ADD_COMMENT_FAILURE:
       return {
         ...state,
         addCommentError: action.error,
         addCommentLoading: false,
+      };
+      case REMOVE_COMMENT_REQUEST:
+      return {
+        ...state,
+        removeCommentLoading: true,
+      };
+    case REMOVE_COMMENT_SUCCESS:
+        const post = action.payload.postId;
+        const thisComment = action.payload.commentId;
+      return {
+        ...state,
+        removeCommentLoading: false,
+        post = post.comments.filter(comment => comment.id !== thisComment)
+      };
+    case REMOVE_COMMENT_FAILURE:
+      return {
+        ...state,
+        addCommentError: action.error,
+        removeCommentLoading: false,
       };
     default:
       return state;
