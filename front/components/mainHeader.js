c347ca9a361a572359f64e10b97ea3aa74c1cc7c
yujin mac <kimyu28jin@gmail.com>
2021-02-02 17:14:34 +0900

    filter menu 기본 스타일 완성

--- front/components/mainHeader.js
+++ front/components/mainHeader.js
@@ -2,8 +2,46 @@ import { AiOutlineHome } from "react-icons/ai";
 import Logo from "./logo";
 import Link from "next/link";
 import RightMenu from "./rightMenu";
+import { Switch } from "antd";
+import { useRef, useState } from "react";
+import styled from "styled-components";
+
+const PostfilterWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  z-index: 98;
+  bottom: 0;
+  background: #fff;
+  padding: 1em;
+  white-space: nowrap;
+  overflow-x: auto;
+  display: flex;
+  justify-content: space-around;
+
+  /* -webkit-transform: translateZ(0px);
+  -webkit-transform: translate3d(0, 0, 0);
+  -webkit-perspective: 1000; */
+`;
 
 const MainHeader = () => {
+  const [secondMenuY, setSecondMenuY] = useState(false);
+  const filterPostMenu = useRef();
+
+  const onClick = (checked) => {
+    console.log(checked);
+    console.log(filterPostMenu);
+
+    if (secondMenuY) {
+      filterPostMenu.current.style.transition = "0.5s";
+      filterPostMenu.current.style.transform = "translateY(0)";
+      setSecondMenuY(false);
+    } else {
+      filterPostMenu.current.style.transition = "0.5s";
+      filterPostMenu.current.style.transform = "translateY(52px)";
+      setSecondMenuY(true);
+    }
+  };
+
   return (
     <div
       style={{
@@ -23,6 +61,7 @@ const MainHeader = () => {
           textAlign: "center",
           width: "100%",
           background: "#fff",
+          zIndex: "99",
         }}
       >
         <Link href="/">
@@ -36,14 +75,29 @@ const MainHeader = () => {
                 transform: "translateY(-50%)",
               }}
             />
+            <Switch
+              defaultChecked={false}
+              style={{
+                fontSize: "1.5rem",
+                position: "absolute",
+                left: "calc(6% + 35px)",
+                top: "50%",
+                transform: "translateY(-50%)",
+              }}
+              size="small"
+              onClick={onClick}
+            />
           </a>
         </Link>
         <Logo style={{ fontSize: "1.8em" }} />
         <RightMenu />
       </div>
-      <div style={{ position: "absolute", width: "100%", bottom: "-22px" }}>
-        second menu
-      </div>
+      <PostfilterWrapper ref={filterPostMenu}>
+        <span>All</span>
+        <span>Followings</span>
+        <span>Followers</span>
+        <span>My</span>
+      </PostfilterWrapper>
     </div>
   );
 };
