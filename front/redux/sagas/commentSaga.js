d86fad60bfd197952219f10cd2189c20706308d2
yujin mac <kimyu28jin@gmail.com>
2021-02-02 20:00:20 +0900

    commentSaga 작성

--- front/redux/sagas/commentSaga.js
+++ front/redux/sagas/commentSaga.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 import { all, call, fork, put, takeEvery } from "redux-saga/effects";
-import { ADD_COMMENT_FAILURE, ADD_COMMENT_SUCCESS } from "../types";
+import {
+  ADD_COMMENT_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  REMOVE_COMMENT_FAILURE,
+  REMOVE_COMMENT_REQUEST,
+  REMOVE_COMMENT_SUCCESS,
+} from "../types";
 
 const addCommentAPI = (payload) => {
   return axios.post(`api/post/${payload.postId}/comment`, payload);
@@ -21,10 +28,32 @@ function* addComment(action) {
   }
 }
 
+const removeCommentAPI = (payload) => {
+  return axios.delete(`api/post/${payload.postId}/comment`, payload);
+};
+
+function* removeComment(action) {
+  try {
+    const result = yield call(removeCommentAPI, action.payload);
+    yield put({
+      type: REMOVE_COMMENT_SUCCESS,
+      payload: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: REMOVE_COMMENT_FAILURE,
+      payload: err.response,
+    });
+  }
+}
+
 function* watchAddComment() {
-  yield takeEvery(addComment);
+  yield takeEvery(ADD_COMMENT_REQUEST, addComment);
+}
+function* watchRemoveComment() {
+  yield takeEvery(REMOVE_COMMENT_REQUEST, removeComment);
 }
 
 export default function* commentSaga() {
-  yield all([fork(watchAddComment)]);
+  yield all([fork(watchAddComment)], [fork(watchRemoveComment)]);
 }
