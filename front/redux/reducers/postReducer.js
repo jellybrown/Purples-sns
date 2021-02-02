abba9400838b1b7716da7728b80a6397a002e180
yujin mac <kimyu28jin@gmail.com>
2021-02-02 18:09:48 +0900

    postReducer 작성

--- front/redux/reducers/postReducer.js
+++ front/redux/reducers/postReducer.js
@@ -1,11 +1,78 @@
 import { HYDRATE } from "next-redux-wrapper";
+import {
+  ADD_POST_FAILURE,
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  REMOVE_POST_FAILURE,
+  REMOVE_POST_REQUEST,
+  REMOVE_POST_SUCCESS,
+} from "../types";
 
-const initialState = {};
+const initialState = {
+  posts: [
+    {
+      id: 1,
+      nickName: "abcd",
+      content: "ddddㅎㅎㅎ",
+    },
+    {
+      id: 2,
+      nickName: "zzzz",
+      content: "ㅎ하ㅏ하하핳",
+    },
+  ],
+  addPostLoading: false,
+  addPostDone: false,
+  addPostError: null,
+  removePostLoading: false,
+  removePostDone: false,
+  removePostError: null,
+};
 
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case HYDRATE:
       return { ...state, ...action.payload };
+    case ADD_POST_REQUEST:
+      return {
+        ...state,
+        addPostLoading: true,
+      };
+    case ADD_POST_SUCCESS:
+      return {
+        ...state,
+        posts: [...posts, action.payload],
+        addPostLoading: false,
+        addPostDone: true,
+      };
+    case ADD_POST_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        addPostLoading: false,
+        addPostError: true,
+      };
+    case REMOVE_POST_REQUEST:
+      return {
+        ...state,
+        removePostLoading: true,
+      };
+    case REMOVE_POST_SUCCESS:
+      return {
+        ...state,
+        posts: posts.filter((post) => post.id !== action.payload),
+        removePostLoading: false,
+        removePostDone: true,
+      };
+    case REMOVE_POST_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        removePostLoading: false,
+        removePostError: true,
+      };
+    default:
+      return state;
   }
 };
 
