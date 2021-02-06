cd4830054189808c962fafe1949e7a63a44225e0
yujin mac <kimyu28jin@gmail.com>
2021-02-06 14:20:39 +0900

    comment, post saga 통합

--- front/redux/sagas/postSaga.js
+++ front/redux/sagas/postSaga.js
@@ -47,13 +47,62 @@ function* removePost(action) {
   }
 }
 
+const addCommentAPI = (payload) => {
+  return axios.post(`api/post/${payload.postId}/comment`, payload);
+};
+
+function* addComment(action) {
+  try {
+    const result = yield call(addCommentAPI, action.payload);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      payload: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      payload: err.response,
+    });
+  }
+}
+
+const removeCommentAPI = (payload) => {
+  return axios.delete(`api/post/${payload.postId}/comment`, payload);
+};
+
+function* removeComment(action) {
+  try {
+    const result = yield call(removeCommentAPI, action.payload);
+    yield put({
+      type: REMOVE_COMMENT_SUCCESS,
+      payload: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: REMOVE_COMMENT_FAILURE,
+      payload: err.response,
+    });
+  }
+}
+
 function* watchAddPost() {
   yield takeEvery(ADD_POST_REQUEST, addPost);
 }
 function* watchRemovePost() {
   yield takeEvery(REMOVE_POST_REQUEST, removePost);
 }
+function* watchAddComment() {
+  yield takeEvery(ADD_COMMENT_REQUEST, addComment);
+}
+function* watchRemoveComment() {
+  yield takeEvery(REMOVE_COMMENT_REQUEST, removeComment);
+}
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)], [fork(watchRemovePost)]);
+  yield all(
+    [fork(watchAddPost)],
+    [fork(watchRemovePost)],
+    [fork(watchAddComment)],
+    [fork(watchRemoveComment)]
+  );
 }
