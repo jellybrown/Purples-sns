d3f91c7a7c760a7eff214cf412fbd6f827058eb5
yujin mac <kimyu28jin@gmail.com>
2021-02-13 15:00:59 +0900

    postForm 작성

--- front/components/postForm.js
+++ front/components/postForm.js
@@ -1,32 +1,77 @@
 import { Button, Input, Modal } from "antd";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
+import Slick from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import { useDispatch } from "react-redux";
+import { ADD_POST_REQUEST } from "../redux/types";
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  max-height: 300px;
+`;
 
 const PostForm = () => {
+  const dispatch = useDispatch();
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [userFile, setUserFile] = useState({});
+
   const [previewImageUrl, setPreviewImageUrl] = useState([]);
+  const [userText, setUserText] = useState("");
+  const [userPhoto, setUserPhoto] = useState([]);
   let data = [];
 
-  useEffect(() => {}, [previewImageUrl]);
+  const fileRef = useRef();
+  const formRef = useRef();
+
+  const onChangeText = (e) => {
+    setUserText(e.target.value);
+  };
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
+  const imageUploader = (photo) => {
+    const formData = new FormData();
+    formData.append("file", photo);
+    return formData;
+  };
+
+  // 글 작성시 text,photo 정보를 dispatch하고, 모달을 닫는 함수 (handleOk)
   const handleOk = () => {
+    console.log(userText);
+    let photoInfo;
+    let imageFile;
+    if (userPhoto.length > 1) {
+      photoInfo = userPhoto.map((photos) => {
+        imageFile = imageUploader(photos);
+      });
+    } else {
+      imageFile = imageUploader(userPhoto);
+    }
+    dispatch({
+      type: ADD_POST_REQUEST,
+      payload: {
+        text: userText,
+        photo: imageFile,
+      },
+    });
     setIsModalVisible(false);
   };
 
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  const fileRef = useRef();
-  const previewRef = useRef();
+
   const onPhotoUpload = () => {
     fileRef.current.click();
   };
 
+  // 이미지 프리뷰를 위한 함수 (onChangePhoto)
+  // 파일정보는 state로 관리함 (userPhoto)
   const onChangePhoto = (e) => {
     let fileList = e.target.files;
 
@@ -40,6 +85,31 @@ const PostForm = () => {
         setPreviewImageUrl(Object.assign([], data));
       };
     }
+
+    setUserPhoto(e.target.files);
+  };
+
+  // 프리뷰 영역 slick arrow (prev, next)
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const NextArr = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <GrNext
+        className={className}
+        style={{ ...style, display: "block", transform: "scale(1.5)" }}
+        onClick={onClick}
+      />
+    );
+  };
+  const PrevArr = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <GrPrevious
+        className={className}
+        style={{ ...style, display: "block", transform: "scale(1.5)" }}
+        onClick={onClick}
+      />
+    );
   };
 
   return (
@@ -71,8 +141,17 @@ const PostForm = () => {
         onCancel={handleCancel}
         cancelText="취소"
       >
-        <Input.TextArea style={{ marginBottom: "2rem" }} />
-        <Button onClick={onPhotoUpload}>사진 선택</Button>
+        <Input.TextArea
+          value={userText}
+          onChange={onChangeText}
+          style={{ marginBottom: "2rem" }}
+        />
+        <Button
+          onClick={onPhotoUpload}
+          style={{ display: "block", marginBottom: "20px" }}
+        >
+          사진 선택
+        </Button>
         <input
           style={{ display: "none" }}
           type="file"
@@ -80,16 +159,32 @@ const PostForm = () => {
           multiple={true}
           onChange={onChangePhoto}
         />
-        {previewImageUrl
-          ? previewImageUrl.map((imageUrl) => {
-              return (
-                <img
-                  style={{ width: "300px", height: "300px" }}
-                  src={imageUrl}
-                />
-              );
-            })
-          : null}
+        {previewImageUrl ? (
+          <p style={{ textAlign: "right" }}>미리보기</p>
+        ) : null}
+        <div style={{ position: "relative" }}>
+          <Slick
+            dots={false}
+            initialSlide={0}
+            beforeChange={(slide) => setCurrentSlide(slide)}
+            slidesToShow={1}
+            slidesToScroll={1}
+            autoplay={false}
+            nextArrow={<NextArr />}
+            prevArrow={<PrevArr />}
+          >
+            {previewImageUrl
+              ? previewImageUrl.map((imageUrl) => (
+                  <Wrapper>
+                    <img
+                      style={{ width: "300px", margin: "0 auto" }}
+                      src={imageUrl}
+                    />
+                  </Wrapper>
+                ))
+              : null}
+          </Slick>
+        </div>
       </Modal>
     </>
   );
