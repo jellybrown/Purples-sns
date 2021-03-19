d11fd1b6866f4de6b1a7ea5dcb51fa676f8fa350
yujin mac <kimyu28jin@gmail.com>
2021-03-19 15:26:37 +0900

    [redux-toolkit] UserSlice 작성

---
+++ front/redux/reducers/UserSlice.js
@@ -0,0 +1,104 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  users: [],
+};
+
+export const searchUser = createAsyncThunk(
+  "user/searchUser",
+  async (payload) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (payload.token) {
+      config.headers["x-auth-token"] = payload.token;
+    }
+    return axios.get(
+      `/api/search/${encodeURIComponent(payload.keyword)}`,
+      config
+    );
+  }
+);
+
+export const follow = createAsyncThunk("user/follow", async (payload) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  if (payload.token) {
+    config.headers["x-auth-token"] = payload.token;
+  }
+  return axios.post("/api/follow/addFollow", payload, config);
+});
+
+export const unFollow = createAsyncThunk("user/unfollow", async (payload) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  if (payload.token) {
+    config.headers["x-auth-token"] = payload.token;
+  }
+  return axios.post("/api/follow/removeFollow", payload, config);
+});
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: {
+    // searchUser
+    [searchUser.pending]: (state, action) => {
+      state.loading = true;
+      state.users = [];
+    },
+    [searchUser.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.users = payload.data;
+    },
+    [searchUser.rejected]: (state, action) => {
+      state.loading = false;
+      state.users = [];
+      console.log("searchUser rejected 💣", action);
+    },
+    // follow
+    [follow.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [follow.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.users = [
+        // 이 코드가 filter를 쓰는 이유가 무엇..? 2021/03/19
+        ...state.users.filter((user) => user._id !== payload.data._id),
+        payload.data,
+      ];
+    },
+    [follow.rejected]: (state, action) => {
+      state.loading = false;
+      console.log("follow rejected 💣", action);
+    },
+    // unFollow
+    [unFollow.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [unFollow.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.users = [
+        // 이 코드가 filter를 쓰는 이유가 무엇..? 2021/03/19
+        ...state.users.filter((user) => user._id !== payload.data._id),
+        payload.data,
+      ];
+    },
+    [unFollow.rejected]: (state, action) => {
+      state.loading = false;
+      console.log("unFollow rejected 💣", action);
+    },
+  },
+});
+
+export default userSlice.reducer;
