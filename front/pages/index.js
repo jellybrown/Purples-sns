b4dbe3c47bdeb0ec9966fc3be3d81e0241b812c9
yujin mac <kimyu28jin@gmail.com>
2021-03-22 13:13:07 +0900

    [redux-toolkit] SSR 코드 예시 작성(주석)

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import MainHome from "../components/MainHome";
 import MainLogin from "../components/mainLogin";
+//import {connect, useDispatch} from "react-redux"; 나중에 사용
+//import { wrapper } from "../redux/reducers/store"; 나중에 사용
 
 const Main = ({ isAuthenticated }) => {
   return (
@@ -15,3 +17,38 @@ const Main = ({ isAuthenticated }) => {
 };
 
 export default Main;
+
+// _app.js에 설정한거 여기다 해야하는거 아닐까? 2021/03/22
+// toolkit 할때 설정하기
+
+// 1. 기본형태
+// export const getServerSideProps = wrapper.getServerSideProps(store => async ({req,res, ...etc})=> {
+//   console.log('2. Page.getServerSideProps uses the store to dispatch things');
+
+//   await store.dispatch(loadEmail()); 필요한부분 ex)유저인증 dispatch하기
+//   await store.dispatch(loadUrl());
+//   await store.dispatch(loadLogs());
+// })
+
+// export default connect(state=> state)(Main);
+
+// 2. 쿠키 가져올시 이런식으로 할 수 있음
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   async (context) => {
+//     const cookie = context.req ? context.req.headers.cookie : "";
+//     axios.defaults.headers.Cookie = "";
+//     if (context.req && cookie) {
+//       axios.defaults.headers.Cookie = cookie;
+//     }
+//     context.store.dispatch({
+//       type: LOAD_MY_INFO_REQUEST,
+//     });
+//     context.store.dispatch({
+//       type: LOAD_POSTS_REQUEST,
+//     });
+//     context.store.dispatch(END);
+//     await context.store.sagaTask.toPromise();
+//   }
+// );
+
+// export default Main;
