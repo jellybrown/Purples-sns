5cdcbb96c0e33344442e4deaf1d0df53b297377f
yujin mac <kimyu28jin@gmail.com>
2021-04-14 16:45:18 +0900

    CardComment 컴포넌트 정리
    - 커멘트 삭제영역 제거
    - 상세페이지 이동 추가
    - styled-component 추가

--- front/components/PostCard/CardComment.js
+++ front/components/PostCard/CardComment.js
@@ -1,12 +1,50 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Card, Avatar } from "antd";
-import { useDispatch, useSelector } from "react-redux";
-import { TiMinus } from "react-icons/ti";
 import styled from "styled-components";
 import CommentForm from "../Forms/CommentForm";
 import { timeAgo } from "../../utils/timeAgo";
+import { useRouter } from "next/router";
 
-const StyledCommentMeta = styled(Card.Meta)`
+const CommentsWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 5px;
+  width: 100%;
+  min-height: 200px;
+  max-height: 800px;
+  .comment-count {
+    font-size: 0.85rem;
+    margin-bottom: 1.3em;
+    margin-left: 5px;
+    color: #a3a3a3;
+  }
+  .comment-list {
+    height: 110px;
+    overflow-y: scroll;
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+    &::-webkit-scrollbar {
+      display: none; /* Chrome, Safari, Opera*/
+    }
+  }
+  .comment-item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.8em;
+    cursor: pointer;
+  }
+  .comment-text {
+    margin-left: 1em;
+    font-size: 0.8rem;
+  }
+  .comment-date {
+    margin-left: 20px;
+    font-size: 0.7rem;
+    color: #a3a3a3;
+  }
+`;
+
+const CommentMeta = styled(Card.Meta)`
   display: inline-flex;
   align-items: center;
   overflow: visible;
@@ -15,106 +53,35 @@ const StyledCommentMeta = styled(Card.Meta)`
   }
 `;
 
-const CardCommentBox = styled.div`
-  position: absolute;
-  left: 0;
-  top: 5px;
-  width: 100%;
-  min-height: 200px;
-  max-height: 800px;
-`;
-
-const CommentCount = styled.p`
-  font-size: 0.85rem;
-  margin-bottom: 1.3em;
-  margin-left: 5px;
-  color: #a3a3a3;
-`;
-
-const CommentLists = styled.ul`
-  height: 110px;
-  overflow-y: scroll;
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-  &::-webkit-scrollbar {
-    display: none; /* Chrome, Safari, Opera*/
-  }
-`;
-
-const CommentList = styled.li`
-  display: flex;
-  align-items: center;
-  margin-bottom: 0.8em;
-`;
-
-const CommentText = styled.span`
-  margin-left: 1em;
-  font-size: 0.8rem;
-`;
-
-const CommentDate = styled.span`
-  margin-left: 20px;
-  font-size: 0.7rem;
-  color: #a3a3a3;
-`;
-
 const CardComment = ({ post }) => {
   const { comments } = post;
-  const { token } = useSelector((state) => state.auth.user);
-  const currentUser = useSelector((state) => state.auth.userId);
-  const dispatch = useDispatch();
-
-  const checkMyComment = (comment) => {
-    const result = currentUser === comment.writer._id ? true : false;
-    return result;
-  };
-
-  const deleteComment = (comment) => {
-    //comment id 받아서 삭제하기
-
-    const body = {
-      token,
-      post,
-      comment,
-    };
-    // dispatch({
-    //   type: REMOVE_COMMENT_REQUEST,
-    //   payload: body,
-    // });
-  };
+  const router = useRouter();
 
   return (
-    <CardCommentBox>
-      <CommentCount>{comments.length}개의 댓글이 있습니다.</CommentCount>
-      <CommentLists>
+    <CommentsWrapper>
+      <p className="comment-count">{comments.length}개의 댓글이 있습니다.</p>
+      <ul
+        className="comment-list"
+        onClick={() => router.push(`/post/${post._id}`)}
+      >
         {comments &&
           comments.map((comment) => {
             return (
-              <CommentList>
-                <StyledCommentMeta
+              <li className="comment-item">
+                <CommentMeta
                   avatar={
                     <Avatar size="small" src={comment.writer.profileImageUrl} />
                   }
                   title={comment.writer.name}
                 />
-                <CommentText>{comment.contents}</CommentText>
-                <CommentDate>{timeAgo(comment.date)}</CommentDate>
-                {checkMyComment(comment) ? (
-                  <TiMinus
-                    onClick={() => deleteComment(comment)}
-                    style={{
-                      fontSize: "1.5rem",
-                      marginLeft: "3rem",
-                      cursor: "pointer",
-                    }}
-                  />
-                ) : null}
-              </CommentList>
+                <span className="comment-text">{comment.contents}</span>
+                <span className="comment-date">{timeAgo(comment.date)}</span>
+              </li>
             );
           })}
-      </CommentLists>
+      </ul>
       <CommentForm post={post} />
-    </CardCommentBox>
+    </CommentsWrapper>
   );
 };
 
