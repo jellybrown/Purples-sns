4cebe2e46d6b1368e3ccb0f62f80fa4c2a80acae
yujin mac <kimyu28jin@gmail.com>
2021-09-14 20:36:45 +0900

    Refactor : profile 컴포넌트 tag 수정

--- front/components/MainProfile.js
+++ front/components/MainProfile.js
@@ -3,67 +3,23 @@ import { FaUserCircle } from "react-icons/fa";
 import { Card } from "antd";
 import styled from "styled-components";
 
-const UserInfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 1em;
-
-  .main-profile__img {
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-  }
-  .main-profile__info {
-    margin-left: 20px;
-    span {
-      font-size: 1rem;
-    }
-    p {
-      font-size: 0.8rem;
-      font-weight: 300;
-    }
-  }
-`;
-
 const UserInfo = () => {
   const { name, profileImageUrl } = useSelector((state) => state.auth.user);
 
   return (
     <UserInfoWrapper>
       {profileImageUrl ? (
-        <img className="main-profile__img" src={profileImageUrl} />
+        <UserImg src={profileImageUrl} />
       ) : (
         <FaUserCircle style={{ fontSize: "4rem" }} />
       )}
-      <div className="main-profile__info">
+      <UserName>
         <span>{name}</span>
         <p>{name}님, 반가워요!</p>
-      </div>
+      </UserName>
     </UserInfoWrapper>
   );
 };
-
-const ProfileInfo = styled.div`
-  display: flex;
-  text-align: center;
-  padding: 20px 0;
-
-  .followers,
-  .followings {
-    flex-basis: 50%;
-  }
-  .followers__title,
-  .followings__title {
-    font-family: "Yellowtail";
-    font-size: 1.3rem;
-    margin: 0;
-  }
-  .followers__count,
-  .followings__count {
-    font-family: "Yellowtail";
-    font-size: 3rem;
-  }
-`;
 
 const MainProfile = () => {
   const authUser = useSelector((state) => state.auth.user);
@@ -75,21 +31,61 @@ const MainProfile = () => {
       style={{ width: 320, borderRadius: "30px" }}
     >
       <ProfileInfo>
-        <div className="followers">
-          <p className="followers__title">Followers</p>
-          <span className="followers__count">
-            {authUser && authUser.followerCount}
-          </span>
-        </div>
-        <div className="followings">
-          <p className="followings__title">Followings</p>
-          <span className="followings__count">
-            {authUser && authUser.followCount}
-          </span>
-        </div>
+        <FollowBox>
+          <Title>Followers</Title>
+          <Count>{authUser && authUser.followerCount}</Count>
+        </FollowBox>
+        <FollowBox>
+          <Title>Followings</Title>
+          <Count>{authUser && authUser.followCount}</Count>
+        </FollowBox>
       </ProfileInfo>
     </Card>
   );
 };
 
 export default MainProfile;
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1em;
+`;
+
+const UserImg = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+`;
+
+const UserName = styled.div`
+  margin-left: 20px;
+  span {
+    font-size: 1rem;
+  }
+  p {
+    font-size: 0.8rem;
+    font-weight: 300;
+  }
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  text-align: center;
+  padding: 20px 0;
+`;
+
+const FollowBox = styled.div`
+  flex-basis: 50%;
+`;
+
+const Title = styled.p`
+  font-family: "Yellowtail";
+  font-size: 1.3rem;
+  margin: 0;
+`;
+
+const Count = styled.span`
+  font-family: "Yellowtail";
+  font-size: 3rem;
+`;
